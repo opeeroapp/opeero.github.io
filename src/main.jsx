f98eaf34836a7ed9d032971c1dbc8b039067768d
Kaushik Mahorker <kmahorker@gmail.com>
2022-10-07 22:04:43 -0700

    update config

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   define: {
     'process.env': process.env
+
   },
+  base: '/website/',
   css: {
     postcss,
   },
